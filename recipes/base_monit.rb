65966b8e8b863244ee0c6757b7018d3253df3d42
Igal Koshevoy <igal@pragmaticraft.com>
2008-12-26 14:59:44 -0800

    Added base_monit recipe.

---
+++ recipes/base_monit.rb
@@ -0,0 +1,29 @@
+# Setup the Monit health monitor
+#
+# NOTE: You must create a "dist/etc/monit/monitrc" file to install Monit. A
+# sample is available as "dist/etc/monit/monitrc.sample"
+
+target_monitrc = "/etc/monit/monitrc"
+source_monitrc = dist+target_monitrc
+
+raise "Couldn't find #{source_monitrc}, for sample see #{source_monitrc}.sample" unless File.exist?(source_monitrc)
+
+modified = package_manager.install 'monit'
+
+# Install configuration file
+modified |= cp source_monitrc, target_monitrc
+
+# Enable startup
+modified |= \
+  edit :file => "/etc/default/monit" do
+    replace /^startup=0/, "startup=1"
+  end
+
+# Run service
+if modified
+  if pmatch? :name => :monit
+    service_manager.tell :monit, "force-reload"
+  else
+    service_manager.start :monit
+  end
+end
