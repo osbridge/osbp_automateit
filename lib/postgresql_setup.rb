e1898b2fb44d1e9156bff77be1717396d9c1745e
Igal Koshevoy <igal@pragmaticraft.com>
2008-12-28 03:49:12 -0800

    Documented postgresql_setup.rb.

--- lib/postgresql_setup.rb
+++ lib/postgresql_setup.rb
@@ -1,25 +1,39 @@
-# Setup PostgreSQL database
+# = PostgreSQLSetup
+#
+# Setup PostgreSQL database.
+#
+# NOTE: Requires that the 'dbi' Ruby package be installed with PostgreSQL bindings.
 class ::PostgreSQLSetup
   include AutomateIt::Constants
 
+  # Default database that's always available, e.g., "template1"
   attr_accessor :default_db
+  # Database connection handle
   attr_accessor :dbh
+  # Database administrator's username
   attr_accessor :dba
+  # AutomateIt interpreter object
   attr_accessor :interpreter
 
+  # Initialize a setup object.
+  #
+  # Options:
+  # * :interpreter => AutomateIt interpreter object. Required.
+  # * :default_db => Default database that's always available. Defaults to "template1".
+  # * :dba => Database administrator's username. Defaults to "postgres".
   def initialize(opts={})
     require 'dbi'
-
-    @interpreter = opts[:interpreter] or raise ArgumentError.new(":interpreter not specified")
-    @interpreter.include_in(self, %w[writing? sh preview? log])
-    @default_db = opts[:default_db] || "template1"
-    @dba = opts[:dba] || "postgres"
+    self.interpreter = opts[:interpreter] or raise ArgumentError.new(":interpreter not specified")
+    self.interpreter.include_in(self, %w[writing? sh preview? log])
+    self.default_db = opts[:default_db] || "template1"
+    self.dba = opts[:dba] || "postgres"
   end
 
+  # Connect to the database
   def connect
     return dbh if dbh and dbh.connected?
     begin
-      return @dbh = DBI.connect("dbi:Pg:dbname=#{default_db}")
+      return self.dbh = DBI.connect("dbi:Pg:dbname=#{default_db}")
     rescue DBI::OperationalError => e
       if writing?
         raise e
@@ -29,6 +43,7 @@ class ::PostgreSQLSetup
     end
   end
 
+  # Connect to the database and add a superuser if needed
   def connect_or_add_superuser
     begin
       tried = false
@@ -47,15 +62,19 @@ class ::PostgreSQLSetup
     end
   end
 
+  # Connected to database?
   def connected?
     return dbh && dbh.connected?
   end
 
+  # Close database connection.
   def close
     return false unless connected?
     return dbh.disconnect
   end
 
+  # Execute a line of +sql+. The +params+ as defined by DBI's statement handle
+  # #execute method, which are typically positional arguments.
   def execute(sql, *params)
     connect
     return nil if not connected? and preview?
@@ -72,27 +91,36 @@ class ::PostgreSQLSetup
     end
   end
 
+  # Fetch data from database using +sql+. The +params+ as defined by DBI's
+  # statement handle #execute method, which are typically positional arguments.
   def fetch(sql, *params)
     rs = execute(sql, *params)
     return [] if preview? and not rs
     return rs.fetch_all
   end
 
+  # Return an array of column names for a given SQL statement.
   def columns(sql)
     return dbh.execute(sql).column_names
   end
 
+  # Does the +user+ have superuser privileges?
   def superuser?(user)
     rs = fetch "select * from pg_user where usename = ?", user
     return false if rs.empty?
     return rs.first['usesuper']
   end
 
+  # Does the +user+ exist in the database?
   def user?(user)
     rs = fetch "select * from pg_user where usename = ?", user
     return ! rs.empty?
   end
 
+  # Add +user+ to the database.
+  #
+  # Options:
+  # * :password => Password to associate with user.
   def add_user(user, opts={})
     return false if user? user
     sql = "create user #{user}"
@@ -101,34 +129,40 @@ class ::PostgreSQLSetup
     fetch sql if writing?
   end
 
+  # Remove +user+ from database.
   def remove_user(user)
     return false unless user? user
     log.info PNOTE+"Removed PostgreSQL user: #{user}"
     fetch "drop user #{user}" if writing?
   end
 
+  # Grant superuser privileges to +user+.
   def grant_superuser(user)
     return false if superuser? user
     log.info PNOTE+"Granted superuser rights to PostgreSQL user: #{user}"
     fetch "alter user #{user} createuser" if writing?
   end
 
+  # Revoke superuser privileges from +user+.
   def revoke_superuser(user)
     return false unless superuser? user
     log.info PNOTE+"Revoked superuser rights from PostgreSQL user: #{user}"
     fetch "alter user #{user} nocreateuser" if writing?
   end
 
+  # Is a procedural +language+ installed?
   def language?(language)
     rs = fetch "select * from pg_language where lanname = ?", language
     return ! rs.empty?
   end
 
+  # Add a procedural +language+.
   def add_language(language)
     log.info PNOTE+"Added PostgreSQL language: #{language}"
     fetch "create language #{language}" if writing?
   end
 
+  # Remove a procedural +language+.
   def remove_language(language)
     log.info PNOTE+"Removed PostgreSQL language: #{language}"
     fetch "drop language #{language}" if writing?
