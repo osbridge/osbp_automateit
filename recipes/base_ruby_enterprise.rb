dbce4544e30afe0648626d5a66d330525e77bded
Igal Koshevoy <igal@pragmaticraft.com>
2008-12-28 02:26:44 -0800

    Rewrote base_ruby_enterprise.rb to use binary package.

--- recipes/base_ruby_enterprise.rb
+++ recipes/base_ruby_enterprise.rb
@@ -1,47 +1,20 @@
 # Setup Phusion Ruby Enterprise Edition
-#
-# NOTE: This depends on the "base_stow" recipe.
-
-#---[ Setup ]-----------------------------------------------------------
 
 # URL from distro site at http://www.rubyenterpriseedition.com/download.html
-url = "http://rubyforge.org/frs/download.php/48623/ruby-enterprise-1.8.6-20081215.tar.gz"
+url = "http://rubyforge.org/frs/download.php/48625/ruby-enterprise_1.8.6-20081215-i386.deb"
 
-#---[ Install ]---------------------------------------------------------
-
-prefix  = "ruby-enterprise"
-archive = File.basename(url)
-package = File.basename(archive, ".tar.gz")
-stowage = File.join(stow_dir, package)
-
-unless File.directory?(stowage)
-  package_manager.install <<-HERE
-    zlib1g-dev
-    libssl-dev
-    libsqlite3-dev
-  HERE
-
-  archive_fqfn = File.join("/tmp", archive)
-  unless File.exists?(archive_fqfn)
-    # TODO Make AutomateIt's DownloadManager only download when needed
-    download url, archive_fqfn
-  end
-
-  mktempdircd do
-    sh "tar xvfz #{archive_fqfn}"
-    cd package do
-      sh "./installer --auto #{stowage}"
-    end
-  end
-
-  cd stow_dir do
-    sh "stow --delete #{prefix}-*"
-    sh "stow #{package}"
+package = File.basename(url)
+package_dir = "/tmp"
+package_fqfn = File.join(package_dir, package)
+unless package_manager.installed?("ruby-enterprise")
+  cd package_dir do
+    download url unless File.exists?(package)
+    package_manager.install({"ruby-enterprise" => package_fqfn}, :with => :dpkg)
   end
 end
 
 # Use existing gems
-edit "/usr/local/lib/ruby/site_ruby/1.8/rubygems.rb" do
+edit "/opt/ruby-enterprise/lib/ruby/site_ruby/1.8/rubygems.rb" do
   manipulate do |buffer|
     unless contains? /GEM_PATH hacked/
       buffer.gsub!(
