3780cf7a8825767186753a87d0ed9703d2b569f8
Igal Koshevoy <igal@pragmaticraft.com>
2008-12-28 08:57:38 -0800

    Added wget to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 # Install Ruby, RubyGems and AutomateIt
 apt-get update
 apt-get install -y ruby1.8 ruby1.8-dev irb1.8 ri1.8 rdoc1.8 \
-    libopenssl-ruby1.8 libxml-ruby
+    libopenssl-ruby1.8 libxml-ruby wget
 ln -s /usr/bin/ruby1.8 /usr/bin/ruby
 pushd /tmp
 wget http://rubyforge.org/frs/download.php/45905/rubygems-1.3.1.tgz
