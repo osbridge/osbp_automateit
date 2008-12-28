d08a5caf9203a537504516059df316e70e91d70c
Igal Koshevoy <igal@pragmaticraft.com>
2008-12-28 07:05:16 -0800

    Fixed base_ruby, added install of ruby1.8 and ruby1.8-dev. Symlinked erb.

--- recipes/base_ruby.rb
+++ recipes/base_ruby.rb
@@ -1,6 +1,8 @@
 # Setup Ruby
 
 package_manager.install <<-HERE
+  ruby1.8
+  ruby1.8-dev
   irb1.8
   ri1.8
   rdoc1.8
@@ -10,3 +12,4 @@ HERE
 ln_sf '/usr/bin/irb1.8',  '/usr/bin/irb'
 ln_sf '/usr/bin/ri1.8',   '/usr/bin/ri'
 ln_sf '/usr/bin/rdoc1.8', '/usr/bin/rdoc'
+ln_sf '/usr/bin/erb1.8', '/usr/bin/erb'
