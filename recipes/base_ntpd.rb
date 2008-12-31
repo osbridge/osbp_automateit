0ee3e6f1cf4b860bf5b2f33fe18289ebb5a23977
Igal Koshevoy <igal@pragmaticraft.com>
2008-12-31 11:15:36 -0800

    Do not use NTP on OpenVZ VMs

--- recipes/base_ntpd.rb
+++ recipes/base_ntpd.rb
@@ -1,9 +1,12 @@
 # Setup ntpd for keeping time in sync
 
-modified = package_manager.install %w[ntpdate ntp]
+# Do not use NTP on OpenVZ VMs, it can't set the clock.
+if !File.exist?('/proc/vz/veinfo') || File.read('/proc/vz/veinfo').strip.empty?
+  modified = package_manager.install %w[ntpdate ntp]
 
-# Sync the time
-sh "/etc/network/if-up.d/ntpdate" if modified
+  # Sync the time
+  sh "/etc/network/if-up.d/ntpdate" if modified
 
-service_manager.enable "ntp"
-service_manager.start "ntp"
+  service_manager.enable "ntp"
+  service_manager.start "ntp"
+end
