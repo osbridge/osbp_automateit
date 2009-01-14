b02c28371c3ffc5d3d3975caf7563424acf5dbb5
Igal Koshevoy <igal@pragmaticraft.com>
2009-01-14 04:25:27 -0800

    Renamed bridgepdx.org-wordpress to bridgepdx_wordpress. Added migration instructions.

--- recipes/my_bridgepdx_wordpress.rb
+++ recipes/my_bridgepdx_wordpress.rb
@@ -5,9 +5,31 @@
 # - my_reown.rb
 # - my_bridgepdx_user.rb
 
+=begin
+# Instructions for migration:
+
+## Dump existing database to file
+mysqldump --add-locks --create-options --disable-keys --extended-insert --quick --set-charset --user=wordpress wordpress > /tmp/wordpress.sql
+
+## Create new database, grant rights, and specify hostname via mysql
+create database bridgepdx_wordpress;
+grant all on bridgepdx_wordpress.* to 'bridgepdx'@'localhost' identified by 'FIXME';
+
+# Restore database
+mysql bridgepdx_wordpress < /tmp/wordpress.sql
+
+# Fix urls via mysql
+use bridgepdx_wordpress;
+update wp_options set option_value='http://FIXME/' where option_name='siteurl';
+update wp_options set option_value='http://FIXME/' where option_name='home';
+
+# Fix configuration
+wp-config.php
+=end
+
 # Variables
 user = "bridgepdx"
-sitename = "bridgepdx.org-wordpress"
+sitename = "bridgepdx_wordpress"
 docroot = "/var/www/#{sitename}"
 site_available = "/etc/apache2/sites-available/#{sitename}"
 site_enabled = "/etc/apache2/sites-enabled/#{sitename}"
