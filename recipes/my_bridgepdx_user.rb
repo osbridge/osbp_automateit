61ebf701f5f414f337f44085b1b70d0ec13b238f
Igal Koshevoy <igal@pragmaticraft.com>
2009-01-29 04:58:18 -0800

    Fixed my_bridgepdx_user, added www-data user to bridgepdx group.

--- recipes/my_bridgepdx_user.rb
+++ recipes/my_bridgepdx_user.rb
@@ -1,3 +1,7 @@
 # Setup a bridgepdx user
 
+# Create user
 account_manager.add_user 'bridgepdx'
+
+# Grant web server rights to user's group
+account_manager.add_groups_to_user(['bridgepdx'], 'www-data')
