5f53046f7a056c1ef4fbacdfbe9b629a8af3e59e
Igal Koshevoy <igal@pragmaticraft.com>
2009-01-29 02:39:39 -0800

    Improved my_apache, added sites-available/default.

--- recipes/my_apache.rb
+++ recipes/my_apache.rb
@@ -1,10 +1,14 @@
 # Setup additional Apache features
 
+# Apache modules to enable
 mods = %w[
   vhost_alias
   rewrite
 ]
 
+#-----------------------------------------------------------------------
+
+# Enable modules
 mods.each do |mod|
   %w[conf load].each do |ext|
     source = "/etc/apache2/mods-available/#{mod}.#{ext}"
@@ -14,3 +18,14 @@ mods.each do |mod|
     end
   end
 end
+#
+# Setup default site
+sitename = "default"
+site_available = "/etc/apache2/sites-available/#{sitename}"
+site_enabled = "/etc/apache2/sites-enabled/#{sitename}"
+modified = false
+modified |=  cp dist+site_available, site_available, :user => 'root', :group => 'root', :mode => 0444
+# Don't symlink because default is already linked as 000-default
+
+# Reload apache if needed
+service_manager.tell("apache", "force-reload") if modified
