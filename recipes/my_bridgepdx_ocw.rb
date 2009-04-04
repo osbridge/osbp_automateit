8bb0768bddb12b10df0de691b75567f687e31bc3
Igal Koshevoy <igal@pragmaticraft.com>
2009-04-04 10:28:44 -0700

    Refactored my_bridgepdx_ocw recipe using new apache_manager.

--- recipes/my_bridgepdx_ocw.rb
+++ recipes/my_bridgepdx_ocw.rb
@@ -7,16 +7,12 @@ package_manager.install %w[mbleigh-acts-as-taggable-on right_aws thoughtbot-pape
 user = "bridgepdx"
 sitename = "bridgepdx_ocw"
 docroot = "/var/www/#{sitename}"
-site_available = "/etc/apache2/sites-available/#{sitename}"
-site_enabled = "/etc/apache2/sites-enabled/#{sitename}"
 
 # Setup directory
 mkdir_p(docroot) && chown(user, user, docroot, :recursive => true)
 
 # Setup apache
-modified = false
-modified |=  cp dist+site_available, site_available, :user => 'root', :group => 'root', :mode => 0444
-modified |= ln_sf site_available, site_enabled
+modified = apache_manager.install_site(sitename)
 
 # Reload apache if needed
-service_manager.tell("apache", "force-reload") if modified
+apache_manager.reload if modified
