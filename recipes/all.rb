a463cae7ac13721e66f03fe39b254774c15c9c07
Igal Koshevoy <igal@pragmaticraft.com>
2009-04-14 19:35:50 -0700

    Improved 'all' recipe, added use of :standby tag and my_penwrap recipe.

--- recipes/all.rb
+++ recipes/all.rb
@@ -35,22 +35,27 @@ invoke 'base_package_managers'
 invoke 'base_python_easy_install'
 invoke 'base_java'
 
-# Base services
-invoke 'base_postgresql' # Relies on base_shmem
-invoke 'base_mysql'
-invoke 'base_apache'
-invoke 'base_php5'
-invoke 'base_ruby_enterprise'
-invoke 'base_passenger' # Relies on base_apache, base_ruby_enterprise
-invoke 'base_memcached'
+# Enable/disable penwrap proxy
+invoke 'my_penwrap'
 
-# Customizations
-invoke 'my_packages'
-invoke 'my_apache' # Relies on base_apache
-invoke 'my_ruby' # Relies on base_compilers_and_interpreters
-invoke 'my_bridgepdx_user'
-invoke 'my_bridgepdx_common' # Relies on my_apache, my_bridgepdx_user
-invoke 'my_bridgepdx_wordpress' # Relies on my_bridgepdx_common, my_apache, base_php5, base_mysql, my_bridgepdx_user
-invoke 'my_bridgepdx_ocw' # Relies on my_apache, my_ruby, my_bridgepdx_user
-invoke 'my_bridgepdx_secrets' # Relies on my_apache, base_php5, my_bridgepdx_user
-invoke 'my_reown'
+unless tagged?(:standby)
+  # Base services
+  invoke 'base_postgresql' # Relies on base_shmem
+  invoke 'base_mysql'
+  invoke 'base_apache'
+  invoke 'base_php5'
+  invoke 'base_ruby_enterprise'
+  invoke 'base_passenger' # Relies on base_apache, base_ruby_enterprise
+  invoke 'base_memcached'
+
+  # Customizations
+  invoke 'my_packages'
+  invoke 'my_apache' # Relies on base_apache
+  invoke 'my_ruby' # Relies on base_compilers_and_interpreters
+  invoke 'my_bridgepdx_user'
+  invoke 'my_bridgepdx_common' # Relies on my_apache, my_bridgepdx_user
+  invoke 'my_bridgepdx_wordpress' # Relies on my_bridgepdx_common, my_apache, base_php5, base_mysql, my_bridgepdx_user
+  invoke 'my_bridgepdx_ocw' # Relies on my_apache, my_ruby, my_bridgepdx_user
+  invoke 'my_bridgepdx_secrets' # Relies on my_apache, base_php5, my_bridgepdx_user
+  invoke 'my_reown'
+end
