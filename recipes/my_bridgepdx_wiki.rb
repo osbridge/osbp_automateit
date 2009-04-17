d561a0f350ea86ed8666a0d1f8d9928d1937e6e7
Igal Koshevoy <igal@pragmaticraft.com>
2009-04-17 08:31:34 -0700

    Improved my_bridgepdx_wiki, added Rakefile that dumps/restores database and added it as a cron job.

--- recipes/my_bridgepdx_wiki.rb
+++ recipes/my_bridgepdx_wiki.rb
@@ -1,6 +1,8 @@
 # Setup attendee wiki.
 # NOTE Apache site is managed by the 'my_bridgepdx_wordpress' recipe.
 
+user = "bridgepdx"
+
 # Install dependencies
 package_manager.install <<-HERE
   php5-gmp
@@ -16,3 +18,12 @@ mkdir_p(path) and chperm(path, :user => "bridgepdx", :group => "bridgepdx")
 # path = "/usr/share/php/Auth"; cp(dist+path, path)
 ### Workaround:
 path = "usr/share/php/Auth"; cp(dist+path, "/"+path)
+
+# Add program that can dump database to file
+cpdist("/var/www/bridgepdx_wiki/Rakefile")
+
+# Add task to dump database to file
+edit("/var/spool/cron/crontabs/#{user}", :create => true, :user => user, :group => "crontab", :mode => 0600) do
+  append "# m h  dom mon dow   command"
+  append "17 * * * * (cd /var/www/bridgepdx_wiki && rake --silent dump)"
+end
