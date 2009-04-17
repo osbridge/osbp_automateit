0ef19169826b9b3898567e1e614305582052da6c
root <root@hawthorne.pragmaticraft.com>
2009-04-16 17:29:04 -0700

    Added PHP OpenID library for use with my_bridgepdx_wiki.

--- recipes/my_bridgepdx_wiki.rb
+++ recipes/my_bridgepdx_wiki.rb
@@ -10,3 +10,9 @@ HERE
 # Create directory
 path = "/var/www/bridgepdx_wiki"
 mkdir_p(path) and chperm(path, :user => "bridgepdx", :group => "bridgepdx")
+
+# Install PHP OpenID library, downloaded from http://openidenabled.com/php-openid/
+### FIXME AutomateIt is getting confused because it sees two "//"s and thus isn't matching
+# path = "/usr/share/php/Auth"; cp(dist+path, path)
+### Workaround:
+path = "usr/share/php/Auth"; cp(dist+path, "/"+path)
