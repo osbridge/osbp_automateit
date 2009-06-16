0c35e18161580119aaf28954d0dea0254f4620e1
Igal Koshevoy <igal@pragmaticraft.com>
2009-06-16 04:51:03 -0700

    Improved my_bridgepdx_ocw, included task to dump database.

--- recipes/my_bridgepdx_ocw.rb
+++ recipes/my_bridgepdx_ocw.rb
@@ -27,3 +27,9 @@ modified = apache_manager.install_site(sitename)
 
 # Reload apache if needed
 apache_manager.reload if modified
+
+# Add task to dump database to file
+edit("/var/spool/cron/crontabs/#{user}", :create => true, :user => user, :group => "crontab", :mode => 0600) do
+  append "# m h  dom mon dow   command"
+  append "19 * * * * if test -f /var/www/bridgepdx_ocw/current/Rakefile; then (cd /var/www/bridgepdx_ocw/current && rake RAILS_ENV=production --silent db:raw:dump FILE=/var/www/bridgepdx_ocw/shared/db/production.sql); fi"
+end
