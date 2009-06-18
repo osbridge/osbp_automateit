210d3f1f60f865b2ac24ab738bc920be402bfa48
Igal Koshevoy <igal@pragmaticraft.com>
2009-06-18 06:40:45 -0700

    Improved base_php, added rules to modify upload_max_filesize.

--- recipes/base_php5.rb
+++ recipes/base_php5.rb
@@ -1,8 +1,13 @@
-# Setup PHP5
+# Setup PHP5.
 #
-# NOTE: Can set memory based on "php5#memory_limit" field.
+# This recipe will also modify the PHP5 settings for:
+# - Maximum amount of memory based on the "php5#memory_limit" field, else use
+#   the default value below.
+# - Maximum size of file uploads based on the "php5#upload_max_filesize"
+#   field, else use the default value below.
 
 memory_limit = lookup("php5#memory_limit") rescue "64M"
+upload_max_filesize = lookup("php5#upload_max_filesize") rescue "8M"
 
 package_manager.install <<-HERE
   apache2
@@ -16,15 +21,21 @@ package_manager.install <<-HERE
   php5-sqlite3
 HERE
 
-edit_memory_limit_in = lambda do |filename|
-  edit(filename, :locals => {:memory_limit => memory_limit}) do
+["/etc/php5/cli/php.ini", "/etc/php5/apache2/php.ini"].each do |filename|
+  edit(filename, :locals => {
+      :memory_limit => memory_limit,
+      :upload_max_filesize => upload_max_filesize,
+  }) do
     line = "memory_limit = #{memory_limit}; Maximum amount of memory a script may consume"
     unless contains?(line)
       comment /^memory_limit /
       append line
     end
+
+    line = "upload_max_filesize = #{upload_max_filesize}; # Maximum allowed size for uploaded files."
+    unless contains?(line)
+      comment /^upload_max_filesize /
+      append line
+    end
   end
 end
-
-edit_memory_limit_in.call("/etc/php5/apache2/php.ini")
-edit_memory_limit_in.call("/etc/php5/cli/php.ini")
