196e3d2aab35740f66abc02a783d88c2815a370b
Igal Koshevoy <igal@pragmaticraft.com>
2009-09-16 00:34:29 -0700

    Fixed base_ruby_enterprise so it can install x86_64 version from packages.

--- recipes/base_ruby_enterprise.rb
+++ recipes/base_ruby_enterprise.rb
@@ -1,6 +1,6 @@
 # Setup Ruby Enterprise Edition
 
-if tagged?('i386 | i486 | i586 | i686')
+if tagged?('i386 | i486 | i586 | i686 | x86_64')
   invoke 'base_ruby_enterprise_from_package'
 else
   invoke 'base_ruby_enterprise_from_source'
