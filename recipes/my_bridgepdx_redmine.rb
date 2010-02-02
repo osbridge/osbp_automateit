372d6426b4b5fca38dd0ca01d8701df5c86f0a67
Igal Koshevoy <igal@pragmaticraft.com>
2010-02-01 17:55:10 -0800

    Improved bridgepdx_redmine, added cron jobs to backup database and import emails.

--- recipes/my_bridgepdx_redmine.rb
+++ recipes/my_bridgepdx_redmine.rb
@@ -7,3 +7,10 @@ mkdir_p(docroot) && chown(user, user, docroot, :recursive => true)
 
 modified = apache_manager.install_site(sitename)
 apache_manager.reload if modified
+
+# Add task to import emails and dump database to file
+edit("/var/spool/cron/crontabs/#{user}", :create => true, :user => user, :group => "crontab", :mode => 0600) do
+  append "# m h  dom mon dow   command"
+  append "*/1 * * * * if test -f /var/www/bridgepdx_redmine/current/Rakefile; then (cd /var/www/bridgepdx_redmine/current && rake redmine:my:getemails RAILS_ENV=production); fi"
+  append "19 * * * * if test -f /var/www/bridgepdx_redmine/current/Rakefile; then (cd /var/www/bridgepdx_redmine/current && rake RAILS_ENV=production --silent db:raw:dump FILE=/var/www/bridgepdx_redmine/shared/db/production.sql); fi"
+end
