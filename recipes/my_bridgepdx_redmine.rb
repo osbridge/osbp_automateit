5c785010914ee1cf1f7e3f9dccb2e744a757e89d
Igal Koshevoy <igal@pragmaticraft.com>
2010-02-01 18:07:21 -0800

    Improved bridgepdx_redmine, made updates run in Rake's silent mode.

--- recipes/my_bridgepdx_redmine.rb
+++ recipes/my_bridgepdx_redmine.rb
@@ -11,6 +11,7 @@ apache_manager.reload if modified
 # Add task to import emails and dump database to file
 edit("/var/spool/cron/crontabs/#{user}", :create => true, :user => user, :group => "crontab", :mode => 0600) do
   append "# m h  dom mon dow   command"
-  append "*/1 * * * * if test -f /var/www/bridgepdx_redmine/current/Rakefile; then (cd /var/www/bridgepdx_redmine/current && rake redmine:my:getemails RAILS_ENV=production); fi"
+  delete "*/1 * * * * if test -f /var/www/bridgepdx_redmine/current/Rakefile; then (cd /var/www/bridgepdx_redmine/current && rake redmine:my:getemails RAILS_ENV=production); fi"
+  append "*/1 * * * * if test -f /var/www/bridgepdx_redmine/current/Rakefile; then (cd /var/www/bridgepdx_redmine/current && rake redmine:my:getemails --silent RAILS_ENV=production); fi"
   append "19 * * * * if test -f /var/www/bridgepdx_redmine/current/Rakefile; then (cd /var/www/bridgepdx_redmine/current && rake RAILS_ENV=production --silent db:raw:dump FILE=/var/www/bridgepdx_redmine/shared/db/production.sql); fi"
 end
