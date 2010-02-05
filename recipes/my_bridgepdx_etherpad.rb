dc56b6828dd8b66ea00a310f3335aad3723e1cef
Igal Koshevoy <igal@pragmaticraft.com>
2010-02-05 05:39:26 -0800

    Ugh. Disabling Etherpad because the production server doesn't have enough memory to handle it.

--- recipes/my_bridgepdx_etherpad.rb
+++ recipes/my_bridgepdx_etherpad.rb
@@ -11,9 +11,10 @@ apache_manager.reload if modified
 # Add task to import emails and dump database to file
 edit("/var/spool/cron/crontabs/#{user}", :create => true, :user => user, :group => "crontab", :mode => 0600) do
   append "# m h  dom mon dow   command"
+  # FIXME temporarily disable these because the production server has too little memory to handle Etherpad :(
   # Start the server on boot
-  append "@reboot if test -f /var/www/bridgepdx_etherpad/Rakefile; then cd /var/www/bridgepdx_etherpad; rake --silent start; fi"
+  delete "@reboot if test -f /var/www/bridgepdx_etherpad/Rakefile; then cd /var/www/bridgepdx_etherpad; rake --silent start; fi"
   # Backup the database to a file
-  append "20 * * * * if test -f /var/www/bridgepdx_etherpad/Rakefile; then cd /var/www/bridgepdx_etherpad; rake --silent backup; fi"
+  delete "20 * * * * if test -f /var/www/bridgepdx_etherpad/Rakefile; then cd /var/www/bridgepdx_etherpad; rake --silent backup; fi"
 end
 
