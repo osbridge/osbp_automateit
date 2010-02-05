ab8db79ca55c73de39afd2b9adb555964acd9326
Igal Koshevoy <igal@pragmaticraft.com>
2010-02-05 09:50:10 -0800

    Enabling bridgepdx_etherpad, now that the server memory's been upgraded.

--- recipes/my_bridgepdx_etherpad.rb
+++ recipes/my_bridgepdx_etherpad.rb
@@ -11,10 +11,9 @@ apache_manager.reload if modified
 # Add task to import emails and dump database to file
 edit("/var/spool/cron/crontabs/#{user}", :create => true, :user => user, :group => "crontab", :mode => 0600) do
   append "# m h  dom mon dow   command"
-  # FIXME temporarily disable these because the production server has too little memory to handle Etherpad :(
   # Start the server on boot
-  delete "@reboot if test -f /var/www/bridgepdx_etherpad/Rakefile; then cd /var/www/bridgepdx_etherpad; rake --silent start; fi"
+  append "@reboot if test -f /var/www/bridgepdx_etherpad/Rakefile; then cd /var/www/bridgepdx_etherpad; rake --silent start; fi"
   # Backup the database to a file
-  delete "20 * * * * if test -f /var/www/bridgepdx_etherpad/Rakefile; then cd /var/www/bridgepdx_etherpad; rake --silent backup; fi"
+  append "20 * * * * if test -f /var/www/bridgepdx_etherpad/Rakefile; then cd /var/www/bridgepdx_etherpad; rake --silent backup; fi"
 end
 
