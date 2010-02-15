30d1e656529600ce7cf2f70f32b83753f88fc286
Igal Koshevoy <igal@pragmaticraft.com>
2010-02-15 09:38:13 -0800

    Moved bridgepdx_wiki_2009 to own directory, updated all paths.

--- recipes/my_bridgepdx_wiki_2009.rb
+++ recipes/my_bridgepdx_wiki_2009.rb
@@ -1,14 +1,23 @@
 # Setup attendee wiki.
 # NOTE Apache site is managed by the 'my_bridgepdx_wordpress' recipe.
 
+# The app used to live at another directory
+old_dir = "/var/www/bridgepdx_wiki"
+sitedir = "/var/www/bridgepdx_wiki_2009"
+
+# Move directory over if found
+if mv(old_dir, sitedir)
+  service_manager.tell "apache2", "reload"
+end
+
 # Create directory
-sitedir = "/var/www/bridgepdx_wiki"
 mkdir_p(sitedir) and chperm(sitedir, :user => default_user, :group => default_group)
 
 # Add task to dump database to file
 cronedit(default_user) do
   append "# m h  dom mon dow   command"
-  delete "17 * * * * (cd #{sitedir} && rake --silent dump)"
-  delete "18 * * * * if test -f #{sitedir}/Rakefile; then (cd #{sitedir} && rake --silent dump); fi"
+  delete "17 * * * * (cd #{old_dir} && rake --silent dump)"
+  delete "18 * * * * if test -f #{old_dir}/Rakefile; then (cd #{old_dir} && rake --silent dump); fi"
+  delete "18 * * * * if test -f #{old_dir}/LocalSettings.php; then (cd #{old_dir} && rake --silent dump); fi"
   append "18 * * * * if test -f #{sitedir}/LocalSettings.php; then (cd #{sitedir} && rake --silent dump); fi"
 end
