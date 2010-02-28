d77dd5bf6be5f69acef2fcfd079d2d3d46925280
Igal Koshevoy <igal@pragmaticraft.com>
2010-02-27 18:32:06 -0800

    Improved mysql_manager: added dbi and dbd packages.

--- lib/mysql_manager.rb
+++ lib/mysql_manager.rb
@@ -65,7 +65,9 @@ class MysqlManager
 
   # Install MySQL server, client and libraries.
   def install_packages
-    return(interpreter.package_manager.install <<-HERE)
+    modified = false 
+    
+    modified |= interpreter.package_manager.install <<-HERE
       # Core
       mysql-server
       mysql-client
@@ -79,6 +81,13 @@ class MysqlManager
       libmysql-ruby
       libdbd-mysql-ruby
     HERE
+
+    modified |= interpreter.package_manager.install <<-HERE, :with => :gem
+      dbi
+      dbd-mysql
+    HERE
+
+    return modified
   end
 
   # Start and enable the MySQL server.
