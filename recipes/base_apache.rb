37e83a840b3df8ab7116c5e74677d39f89b88772
Igal Koshevoy <igal@pragmaticraft.com>
2010-03-01 13:36:09 -0800

    Switched base_apache to using ApacheManager.

--- recipes/base_apache.rb
+++ recipes/base_apache.rb
@@ -1,6 +1,3 @@
 # Setup Apache web server
 
-modified = package_manager.install "apache2"
-
-service_manager.enable "apache2"
-service_manager.tell "apache2", "restart" if modified
+apache_manager.install
