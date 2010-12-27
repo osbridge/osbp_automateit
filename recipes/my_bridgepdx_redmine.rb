4268edc5af5e6e0801342a087fabd99f3d083053
Igal Koshevoy <igal@pragmaticraft.com>
2010-12-27 08:57:45 -0800

    Remove bridgepdx_redmine's getemails task

--- recipes/my_bridgepdx_redmine.rb
+++ recipes/my_bridgepdx_redmine.rb
@@ -10,8 +10,6 @@ apache_manager.reload if modified
 # Add task to import emails and dump database to file
 cronedit(default_user) do
   append "# m h  dom mon dow   command"
-  delete "*/1 * * * * if test -f /var/www/bridgepdx_redmine/current/Rakefile; then (cd /var/www/bridgepdx_redmine/current && rake redmine:my:getemails RAILS_ENV=production); fi"
-  delete "*/1 * * * * if test -f /var/www/bridgepdx_redmine/current/Rakefile; then (cd /var/www/bridgepdx_redmine/current && rake redmine:my:getemails --silent RAILS_ENV=production); fi"
-  append "*/1 * * * * if test -f /var/www/bridgepdx_redmine/current/Rakefile; then (cd /var/www/bridgepdx_redmine/current && nice -n19 rake redmine:my:getemails --silent RAILS_ENV=production); fi"
   append "19 * * * * if test -f /var/www/bridgepdx_redmine/current/Rakefile; then (cd /var/www/bridgepdx_redmine/current && rake RAILS_ENV=production --silent db:raw:dump FILE=/var/www/bridgepdx_redmine/shared/db/production.sql); fi"
+  delete /rake redmine:my:getemails/
 end
