5cb8896e323171c0f7471a835f62be7357182bab
Igal Koshevoy <igal@pragmaticraft.com>
2012-05-09 13:57:18 -0700

    my_bridgepdx_wiki_2009: Don't move aside /var/www/bridgepdx_wiki.

--- recipes/my_bridgepdx_wiki_2009.rb
+++ recipes/my_bridgepdx_wiki_2009.rb
@@ -1,22 +1,11 @@
 # Setup attendee wiki.
 # NOTE Apache site is managed by the 'my_bridgepdx_wordpress' recipe.
 
-# The app used to live at another directory
-old_dir = "/var/www/bridgepdx_wiki"
-sitedir = "/var/www/bridgepdx_wiki_2009"
-
-# Move directory over if found
-if mv(old_dir, sitedir)
-  service_manager.tell "apache2", "reload"
-end
-
 # Create directory
+sitedir = "/var/www/bridgepdx_wiki_2009"
 mkdir_p(sitedir) and chperm(sitedir, :user => default_user, :group => default_group)
 
 # Add task to dump database to file
 cronedit(default_user) do
-  delete "17 * * * * (cd #{old_dir} && rake --silent dump)"
-  delete "18 * * * * if test -f #{old_dir}/Rakefile; then (cd #{old_dir} && rake --silent dump); fi"
-  delete "18 * * * * if test -f #{old_dir}/LocalSettings.php; then (cd #{old_dir} && rake --silent dump); fi"
   append "18 * * * * if test -f #{sitedir}/LocalSettings.php; then (cd #{sitedir} && rake --silent dump); fi"
 end
