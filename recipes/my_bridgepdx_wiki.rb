a5f8958517b72ab5062ddd0616655adb068ec451
Igal Koshevoy <igal@pragmaticraft.com>
2012-05-09 14:16:03 -0700

    my_bridgepdx_wiki: Switch to a unified wiki.
    In the past there was a wiki per year. Now there's a single unified wiki.
    * All old recipes are replaced with a single one.
    * New rewrite rules retain access to old URLs.
    * Update cronjobs to add new dumper and remove old dumpers.
    * Recipe abandons old wiki files and removes all access to them, but the
      old files are left behind for manual removal.

--- recipes/my_bridgepdx_wiki.rb
+++ recipes/my_bridgepdx_wiki.rb
@@ -2,11 +2,14 @@
 # NOTE Apache site is managed by the 'my_bridgepdx_wordpress' recipe.
 
 # Create directory
-sitedir = "/var/www/bridgepdx_wiki_2011"
+sitedir = "/var/www/bridgepdx_wiki"
 mkdir_p(sitedir) and chperm(sitedir, :user => default_user, :group => default_group)
 
 # Add task to dump database to file
 cronedit(default_user) do
   append "18 * * * * if test -f #{sitedir}/LocalSettings.php; then (cd #{sitedir} && rake --silent dump); fi"
+
+  # Delete previous cronjobs from back when there was a wiki per year
+  delete %r{18 \* \* \* \* if test -f #{sitedir}_\d{4}/LocalSettings.php; then \(cd #{sitedir}_\d{4} \&\& rake --silent dump\); fi}
 end
 
